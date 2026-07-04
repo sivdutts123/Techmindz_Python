99a6e277f5bd59c99c0d5563814fb23bc1b7dd51
sivdutts123 <sivdutts2003@gmail.com>
2026-07-04 21:10:04 +0530

    Add 12.py and 34.py

---
+++ 12.py
@@ -0,0 +1,42 @@
+# recursion
+
+# def counttozero(n):
+#     print(n)
+#     if n == 0:
+#         return
+#     return counttozero(n-1)
+
+
+# counttozero(10)
+
+# sum
+# def sum(n):
+#     if n == 0:
+#         return 0
+#     return n + sum(n - 1)
+# print(sum(15))
+
+# scope of a variable
+# area in which it is recognized
+# name = "risla" #global scope
+# def myname():
+#     name = "sreeja" #local scope
+# print(name)
+
+# name = "risal"
+# def myname():
+#     name = "Sreeja"
+#     def nickname():
+#         name = "yazil"
+#         print(name)
+#     nickname()
+
+# myname()
+
+# import md
+# from md import factorial,name as name1,add2
+
+# name = "kumar"
+# print(factorial(5))
+# print(name1)
+# print(md.add2(3,4))
